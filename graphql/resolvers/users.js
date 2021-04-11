1a84c6083572eca53bf490b9b5554ad3ab6b6cfc
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2021-04-11 23:22:34 +0200

    v1.4 | Working on Register & Login Users

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,1 +1,12 @@
-// Users.js
+// Model imported
+const User = require("../../models/User");
+
+module.exports = {
+  Mutation: {
+    register(_, args, context, info) {
+      // TODO Validate user data
+      // TODO Make sure user doesn't already exist
+      // TODO hash password and create an auth token
+    },
+  },
+};
